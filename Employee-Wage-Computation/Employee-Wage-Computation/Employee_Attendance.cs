5e3b9cc4c94b57749d557f7c3ef9a1a0d49b944b
echo # <priyankapardeshi2312@gmail.com>
2021-05-25 17:14:03 +0530

    Calculated Employee wage for month

--- Employee-Wage-Computation/Employee-Wage-Computation/Employee_Attendance.cs
+++ Employee-Wage-Computation/Employee-Wage-Computation/Employee_Attendance.cs
@@ -14,35 +14,40 @@ namespace Employee_Wage_Computation
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_WAGE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
 
             //Variables
             int employeeHr = 0;
             int totalEmployeeWage = 0;
-            Random objRandom = new Random();
-            
+            int MonthlyWage = 0;
+ 
             //Genarating random number for check employee is present or not
-            int Check = objRandom.Next(0, 3);
-
-            switch (Check)
+            for (int i = 0; i < MAX_WORKING_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    employeeHr = 8;
-                    Console.WriteLine("Employee is full Time");
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Emloyee is part time");
-                    employeeHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    employeeHr=0;
-                    break;
-            }
-
-
-            //calculating Employee wage
-            totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
-            Console.WriteLine("Employee Wage is :"+totalEmployeeWage);
+            Random objRandom = new Random();
+            int Check = objRandom.Next(0, 3);
+            // switch statement            
+                switch (Check)
+                {
+                    case 1:
+                        employeeHr = 8;
+                        Console.WriteLine("Employee is full Time");
+                        break;
+                    case 2:
+                        Console.WriteLine("Emloyee is part time");
+                        employeeHr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        employeeHr = 0;
+                        break;
+                }//close switch
+                totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
+                MonthlyWage = MonthlyWage + totalEmployeeWage; 
+            }//close for
+            //calculating Employee wage for month
+            Console.WriteLine("Monthly total  wage is:"+MonthlyWage);
         }
+        
     }
 }
