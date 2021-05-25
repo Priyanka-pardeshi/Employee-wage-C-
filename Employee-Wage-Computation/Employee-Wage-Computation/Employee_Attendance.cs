5851d2453ef2f471528b2b763d74ce289b32eca3
echo # <priyankapardeshi2312@gmail.com>
2021-05-25 16:20:00 +0530

    Added an Part time and full time Employee and calculate daily wage

--- Employee-Wage-Computation/Employee-Wage-Computation/Employee_Attendance.cs
+++ Employee-Wage-Computation/Employee-Wage-Computation/Employee_Attendance.cs
@@ -11,7 +11,8 @@ namespace Employee_Wage_Computation
         public void EmpPresent()
         {
             //Constants
-            const int IS_PRESENT = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int EMP_WAGE_PER_HR = 20;
 
             //Variables
@@ -20,17 +21,23 @@ namespace Employee_Wage_Computation
             Random objRandom = new Random();
             
             //Genarating random number for check employee is present or not
-            int Check = objRandom.Next(0, 2);
-            if (Check == IS_PRESENT)
+            int Check = objRandom.Next(0, 3);
+            if (Check == IS_FULL_TIME)
             {
                 employeeHr = 8;
-                Console.WriteLine(" Employee Is Present");
+                Console.WriteLine(" Employee Is Full time Employee");
+            }
+            else if (Check == IS_PART_TIME)
+            {
+                employeeHr = 4;
+                Console.WriteLine(" Employee is Part time Employee");
             }
             else
             {
                 employeeHr = 0;
-                Console.WriteLine(" Employee is absent");
+                Console.WriteLine("Employee Is Absent");
             }
+
             //calculating Employee wage
             totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
             Console.WriteLine("Employee Wage is :"+totalEmployeeWage);
